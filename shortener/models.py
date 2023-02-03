5e80d146f87ecbfac06923529e4f1c90cd6f8d25
Abram <israelvictory87@gmail.com>
2023-02-03 03:57:45 +0100

    Feat: created link model

---
+++ shortener/models.py
@@ -0,0 +1,27 @@
+# Stdlib Imports
+from datetime import datetime
+
+# SQLAlchemy Imports
+from sqlalchemy import Column, String, Integer, DateTime
+
+# Own Imports
+from config.database import Base
+
+
+class Link(Base):
+    __tablename__ = "links"
+
+    id = Column(Integer, primary_key=True, index=True)
+    original = Column(String)
+    shortened = Column(String(4))
+    date_created = Column(DateTime, default=datetime.now)
+    date_modified = Column(datetime, onupdate=datetime.now)
+
+    def __str__(cls) -> str:
+        """
+        `__str__` is a special method that returns a string representation of an object.
+
+        :param cls: The class object
+        :return: The shortened version of the original link
+        """
+        return cls.shortened.__str__()
