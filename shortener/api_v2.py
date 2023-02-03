8d1ca87844d5ef0574f5cd108a77c7554cd6a686
Abram <israelvictory87@gmail.com>
2023-02-03 13:42:13 +0100

    Feat: created api v2 views

---
+++ shortener/api_v2.py
@@ -0,0 +1,36 @@
+# FastAPI Imports
+from fastapi import APIRouter, Path, Depends
+from fastapi.responses import RedirectResponse
+
+# SQLAlchemy Imports
+from sqlalchemy.orm import Session
+
+# Own Imports
+from config.deps import get_db
+from shortener.models import Link
+from shortener.services import shorten_link
+from shortener.schemas import CreateLinkSchema
+
+
+# Initialize api router
+router = APIRouter(tags=["API v2"])
+
+
+@router.post("/shorten/", status_code=201)
+async def shorten_url(
+    payload: CreateLinkSchema, db: Session = Depends(get_db)
+):
+    shortened_link = shorten_link()
+    link = Link(original=payload.original, shortened=shortened_link)
+
+    db.add(link)
+    db.commit()
+    db.refresh(link)
+
+    return {"message": "Link shortened!", "data": link}
+
+
+@router.get("/{code}/")
+async def redirect_code(code: str = Path(), db: Session = Depends(get_db)):
+    link = db.query(Link).filter_by(shortened=code).first()
+    return RedirectResponse(link.original)
