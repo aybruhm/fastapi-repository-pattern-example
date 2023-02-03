5c6c342a2df9892c31565672e9fcaa988d18725b
Abram <israelvictory87@gmail.com>
2023-02-03 04:49:12 +0100

    Update: modified startup function and runserver method

--- main.py
+++ main.py
@@ -4,9 +4,24 @@ from uvicorn import run as runserver
 # FastAPI Imports
 from fastapi import FastAPI
 
+# Own Imports
+from shortener.models import create_tables
+from shortener.api import router as api_router
+
 
 # initialize app
 app = FastAPI(title="URL Shortener")
+
+# Include router(s)
+app.include_router(api_router)
+
+
+@app.on_event("startup")
+async def startup():
+    """
+    This function creates the database tables when the server starts.
+    """
+    await create_tables()
 
 
 @app.get("/")
@@ -20,4 +35,4 @@ async def root() -> dict:
 
 
 if __name__ == "__main__":
-    runserver(app, host="0.0.0.0", port=3003, reload=True)
+    runserver("main:app", host="0.0.0.0", port=3003, reload=True)
