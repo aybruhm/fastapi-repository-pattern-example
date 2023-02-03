5660161cf1b2d833d05531f684a6b886eb0e9fb8
Abram <israelvictory87@gmail.com>
2023-02-03 03:49:53 +0100

    Feat: created database module

---
+++ config/database.py
@@ -0,0 +1,19 @@
+# SQLAlchemy Imports
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+
+# Construct database type and engine
+DATABASE_URL = "sqlite:///./shortener.sqlite"
+DATABASE_ENGINE = create_engine(
+    url=DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+# Construct a session maker
+SessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=DATABASE_ENGINE
+)
+
+# Construct a base class for declarative class definitions.
+Base = declarative_base()
